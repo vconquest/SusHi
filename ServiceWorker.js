a871324ab318ee4b342553de4383b811183e31d3
brenden382 <brenden@visualconquest.com>
2025-11-17 13:27:28 -0500

    SusHi Project 1st build - 1:27

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "CKS-SusHi-1.4";
+const contentToCache = [
+    "Build/SusHi_Web.loader.js",
+    "Build/SusHi_Web.framework.js",
+    "Build/SusHi_Web.data",
+    "Build/SusHi_Web.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
